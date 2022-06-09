8b839f2cc60c0a70c172820b96dc67fd64119de3
Amanda Guerra <guerraamanda1@hotmail.com>
2022-06-09 11:01:19 -0300

    Errors configs and Database initialization

--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
+require("express-async-errors")
+
+const database = require("./database/sqlite")
+
+const AppError = require("./utils/AppError")
+
 const express = require("express")
+
 const app = express()
-app.use(express.json())
 
 const routes = require("./routes")
+
+app.use(express.json())
+
 app.use(routes)
+
+database()
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = 3334
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
