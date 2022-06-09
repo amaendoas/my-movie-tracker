800eeb91be6f1e76071833ce31795005fec80ce1
Amanda Guerra <guerraamanda1@hotmail.com>
2022-06-09 09:36:46 -0300

    Start of Users routes and users controller

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
-
 const app = express()
+app.use(express.json())
+
+const routes = require("./routes")
+app.use(routes)
 
 const PORT = 3334
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
