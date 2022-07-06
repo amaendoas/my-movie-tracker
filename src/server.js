1d49d2c2c7cc2dddd6a7ca9912a10e3476915068
Amanda Guerra <guerraamanda1@hotmail.com>
2022-07-06 15:43:28 -0300

    Início integração com o frontend

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 require("express-async-errors");
-
 const migrationsRun = require("./database/sqlite/migrations");
 const uploadConfig = require("./configs/upload")
-
 const AppError = require("./utils/AppError");
 
+const cors = require("cors");
 const express = require("express");
-const app = express();
+const routes = require("./routes");
+
 
 migrationsRun();
 
-
-const routes = require("./routes");
-
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
 
 app.use((error, request, response, next) => {
